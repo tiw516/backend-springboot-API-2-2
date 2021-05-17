4e238b18b02366b468db7ab20a4659605a6c3c70
RobotW <wutiantong1993@gmail.com>
2021-05-17 15:47:28 +0800

    BackEnd

--- fullstack/src/main/java/com/example/demo/user/UserController.java
+++ fullstack/src/main/java/com/example/demo/user/UserController.java
@@ -1,8 +1,11 @@
 package com.example.demo.user;
+
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000"},allowCredentials = "true",allowedHeaders = {"X-Custom-Header"},
+        maxAge = 3600L, methods={RequestMethod.GET,RequestMethod.POST,RequestMethod.HEAD})
 @RequestMapping("/api/users")
 public class UserController {
     private UserRepository userRepository;
@@ -17,7 +20,7 @@ public class UserController {
     public User addUser(@RequestBody User user) {
         return userRepository.save(user);
     }
-    @DeleteMapping(value = "/{uid}")//删
+    @DeleteMapping(value = "delete/{uid}")//删
     public void delUser(@PathVariable("uid") Integer uid
     ) {
         userRepository.deleteById(uid);
